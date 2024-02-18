eaada5bef65a3ad55771dc88c221071b3c1c0b6a
PJotter02 <jkbpttr@gmail.com>
2024-02-18 14:32:37 -0700

    GE04

---
+++ GE04RenterSurveyPotter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define ROWS 7
+#define CATEGORIES 3
+
+
+int Main(void) {
+	int rideSharSurvey[ROWS][CATEGORIES] = { 0,0 };
+	int categoryAverage[CATEGORIES] = { 0 };
+
+
+	return 0;
+}
--- example.c
+++ example.c
@@ -5,6 +5,6 @@
 int main(void) {
 	int a[SIZE] = { 1,3,5,7,0 };
 	printf("%p", a);
-	printf("%p", a[0]);
+	printf("\n%p", a[0]);
 
 }
